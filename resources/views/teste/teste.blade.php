d7aed09b9ccba4456dc6200bf18533ea5f7cc47a
calimanisAll <ygor.calimanis@allcomtelecom.com>
2024-10-07 12:54:40 -0300

    wip

---
+++ resources/views/teste/teste.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+
+<body>
+    <?php
+    echo 'Hello World';
+    ?>
+
+    @foreach (range(1,20) as $number)
+    <li><?= $number ?></li>
+    @endforeach
+
+</body>
+
+</html>
